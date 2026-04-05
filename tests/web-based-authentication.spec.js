ece646526ca03121430d76f18865974b91c3a5d3
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-04-05 12:44:31 -0400

    remove basic auth

--- tests/web-based-authentication.spec.js
+++ tests/web-based-authentication.spec.js
@@ -1,14 +1,14 @@
 import { test } from '@playwright/test';
 
-test('Bypass authentication by embedding the credentials in the URL', async ({ page }) => {
+// test('Bypass authentication by embedding the credentials in the URL', async ({ page }) => {
 
     
-    await page.goto("https://admin:admin@the-internet-5chk.onrender.com/basic_auth");
+//     await page.goto("https://admin:admin@the-internet-5chk.onrender.com/basic_auth");
 
-    await page.waitForTimeout(3000);
+//     await page.waitForTimeout(3000);
 
   
-});
+// });
 
 test("Bypass authentication by encoding the credentials base64 format", async ({
   page,
